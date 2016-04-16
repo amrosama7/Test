3b318917596c0ecab3006cf5632132290367b251
amr osama <amr.osama1995@gmail.com>
2016-04-16 02:07:19 +0200

    ya rab

---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+class Project < ActiveRecord::Base
+		belongs_to :company
+			has_many :works
+has_many :users , :through => :works
+
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+		belongs_to :company
+	has_many :works
+	has_many :projects , :through => :works
+
+
+end
